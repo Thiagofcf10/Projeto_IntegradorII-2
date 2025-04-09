c56c12bba47e0b5151c2cbe08aa7a7efd2225234
thiago felipe <thiagofcf1@gmail.com>
2025-04-09 18:27:43 -0300

    12 add node commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ribvjcjogwukedhhkgbx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpYnZqY2pvZ3d1a2VkaGhrZ2J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5Njk4MDMsImV4cCI6MjA1NjU0NTgwM30.152UN5yiJrWyHIeBnZjSmIhGargSRDTqs1BYF0qohwQ';
-const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+const _supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log('Supabase URL:', _supabase);
 
 
 

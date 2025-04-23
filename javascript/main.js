13dfcbd3f83277f6990bfedd2b0e27eb49dad820
thiago felipe <thiagofcf1@gmail.com>
2025-04-23 18:32:35 -0300

    14 commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,8 @@
+
 const supabaseUrl = 'https://ribvjcjogwukedhhkgbx.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpYnZqY2pvZ3d1a2VkaGhrZ2J4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5Njk4MDMsImV4cCI6MjA1NjU0NTgwM30.152UN5yiJrWyHIeBnZjSmIhGargSRDTqs1BYF0qohwQ';
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
 
 
 ////////////////////////////  caroucel  ///////////////////////////////////////// 
@@ -140,4 +142,3 @@ fetchData().then(() => {
         }
 
         fetchImages();
-
